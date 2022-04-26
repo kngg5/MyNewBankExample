526831f18aaf9ca3d7b983b262688a0f672fa9f1
Yaseen <yasinmamatov12@gmail.com>
2022-04-26 17:08:54 +0600

    Project initialized

---
+++ MyNewBank/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewBank
+{
+    internal class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+        private static int accountNumberSeed = 123456789;
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+
+        }
+    }
+}
---
+++ MyNewBank/Program.cs
@@ -0,0 +1,7 @@
+﻿using MyNewBank;
+
+
+var account = new BankAccount("Yaseen",1000);
+var secondAccount = new BankAccount("Jason",20000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+Console.WriteLine($"Account {secondAccount.Number} was created for {secondAccount.Owner} with {secondAccount.Balance} initial balance.");
